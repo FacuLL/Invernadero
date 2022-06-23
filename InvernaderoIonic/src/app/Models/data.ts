dc2217017e9f321c598ad7cd657d6cacfac8be22
FaustinoM <faustinomor.13@gmail.com>
2022-06-23 09:58:30 -0300

    ionic changes

---
+++ InvernaderoIonic/src/app/Models/data.ts
@@ -0,0 +1,10 @@
+export interface Data {
+    temp?: Number
+    humedad?: Number
+    indCal?: Number
+    humedadsuelo?: Number
+    lux?: Number
+    ventilacion?: Number
+    lampara?: Number
+    riego?: Number
+}
--- InvernaderoIonic/src/app/tab1/tab1.page.ts
+++ InvernaderoIonic/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '../Services/data.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private dataService:DataService) {}
+
+  public temp: Number;
+  public humedad: Number;
+  public indCal: Number;
+  public humedadsuelo: Number;
+  public lux: Number;
+  public ventilacion: boolean;
+  public lampara: boolean;
+  public riego: boolean;
+
+  ngOnInit(): void {
+    this.dataService.getLastData().subscribe(data => {
+      this.temp = data.temp;
+      this.humedad = data.humedad;
+      this.indCal = data.indCal;
+      this.humedadsuelo = data.humedadsuelo;
+      this.lux = data.lux;
+      this.ventilacion = !!data.ventilacion;
+      this.lampara = !!data.lampara;
+      this.riego = !!data.riego;
+    })
+  }
 
 }
