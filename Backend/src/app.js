07f41923963a64e1bfec37afbb07c9545b513d82
Facundo López Llopis <faculopllop@gmail.com>
2022-08-01 22:50:47 -0300

    Seguridad backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,18 +2,23 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
 const cors = require('cors');
+const helmet = require("helmet");
 
 const { register } = require('./controllers/app.controller');
 
 const app = express()
 
-//enviroment variables
+// Puerto
 app.set('port', process.env.PORT || 3000);
+
+// Seguridad
+app.use(helmet());
+app.use(cors({origin:'*'}));
 
 app.use(morgan('dev'));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
-app.use(cors({origin:'*'}));
+
 app.use(require('./routes/app.routes'));
 
 
